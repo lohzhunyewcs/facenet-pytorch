d99a3055c6cca74e36b5113f28e7cd384c9db30e
lohzhunyewcs <lohzhunyewcs@gmail.com>
2020-12-28 10:23:49 +0800

    Added debug message for solver output

--- models/mtcnn.py
+++ models/mtcnn.py
@@ -275,7 +275,10 @@ class MTCNN(nn.Module):
             else:
                 raw_img_size = img.shape[:2]
             boxes = [process_box(self.image_size, box, raw_img_size=raw_img_size) for box in batch_boxes]
-            return None, list(zip(batch_probs, boxes))
+            try:
+                return None, list(zip(batch_probs, boxes))
+            except TypeError:
+                raise Exception(f"{batch_probs=}, {boxes = }")
 
         extra = []
         if return_prob:
